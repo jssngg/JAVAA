2cf67246bb4184f596b1cece9400e74a2e93dd59
jssngg <lankey97@hanyang.ac.kr>
2016-11-26 20:48:11 +0900

    Add files via upload

---
+++ Blank.java
@@ -0,0 +1,16 @@
+package dd;
+import java.util.Scanner;
+public class Blank {
+	public static void main(String [] args){
+		int i = 0;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("x");
+		String
+		a = s.next();
+		String c = a.trim();
+		System.out.println(c);
+		
+	}
+
+}
